b05b001988ba175885d9ab5edbdb8d18d20e57f9
Harry Tom <harrytom2606@gmail.com>
2022-03-26 10:30:43 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# 🤡 Roastra
+
+A simple package to create roasts! Light weight, No Dependencies, Easy to use, Simple and Powerful. Also Funny! You can install this package from [npm](https://npmjs.org/package/roastra). Alternatively, you can use [Web API](https://github.com/just-doodle/roastra-web-api)
+
+###### EXAMPLE
+
+| You are as dreadful as a goatish dozen of petty snake piss |
+| ---------------------------------------------------------- |
+
+### 🔨 Install [npm](https://npmjs.org/package/roastra)
+
+```bat
+npm install roastra
+```
+
+### ⚙️ Usage
+
+1. Import the package:
+
+```js
+import Roast from "roastra";
+```
+
+2. Initilize `Roast` Class with the parameters below
+
+```js
+var roast = new Roastra({
+  name: "Rocky",
+});
+```
+
+| Parameter | Description                                 | type              | Required | Default |
+| --------- | ------------------------------------------- | ----------------- | -------- | ------- |
+| `name`    | Name of the person you want to roast        | <key>string</key> | false    | You     |
+| `plural`  | To set if `name` parameter is plural or not | <key>bool</key>   | false    | false   |
+
+## 🙏 Contribute
+
+You can contribute to this project by adding words to [words.json](words.json) and Creating a PR.
+
+## 💪 Support
+
+Support this project by ⭐ Starring this Repo or buy me a coffee!
+
+<a href="https://www.buymeacoffee.com/harrytom" target="_blank"><img width="200" src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 60px !important;width: 217px !important;" ></a>
---
+++ index.js
@@ -0,0 +1,61 @@
+import wordList from "../words.json" assert { type: "json" };
+class Roastra {
+  constructor(options) {
+    if (options.plural === true) {
+      this.plural = "are";
+    } else {
+      this.plural = "is";
+    }
+
+    if (options.name === false || options.name === undefined) {
+      this.name = "You";
+      this.plural = "are";
+    } else {
+      this.name = options.name;
+    }
+
+    if (options.name.toLowerCase() === "they") {
+      this.plural = "are";
+    }
+
+    this.options = options;
+  }
+  Get(callback) {
+    var t = {
+      adjective: [
+        wordList.adjective[
+          Math.floor(Math.random() * wordList.adjective.length)
+        ],
+        wordList.adjective[
+          Math.floor(Math.random() * wordList.adjective.length)
+        ],
+        wordList.adjective[
+          Math.floor(Math.random() * wordList.adjective.length)
+        ],
+      ],
+      amount:
+        wordList.amount[Math.floor(Math.random() * wordList.amount.length)],
+      animal:
+        wordList.animal[Math.floor(Math.random() * wordList.animal.length)],
+      animal_part:
+        wordList.animal_part[
+          Math.floor(Math.random() * wordList.animal_part.length)
+        ],
+    };
+    var sentance = `${this.name} ${this.plural} ${t.adjective[0]} as a ${t.adjective[1]} ${t.amount} of ${t.adjective[2]} ${t.animal} ${t.animal_part}`;
+
+    var result = {
+      sentance,
+      name: this.options.name,
+      plural: this.options.plural,
+    };
+
+    if (typeof callback === "function") {
+      callback(result);
+    }
+
+    return result;
+  }
+}
+
+export default Roast;
