d57182700a3b0fda8b8aaa89e983777bb0303025
Harry <harrytom2606@gmail.com>
2022-04-20 15:36:20 +0530

    Bugfix. Fixed fallback values error

--- index.js
+++ index.js
@@ -1,60 +1,62 @@
 import wordList from "./words.json" assert { type: "json" };
 class Roastra {
-  constructor(options) {
-    if (options.plural === true) {
-      this.plural = "are";
-    } else {
-      this.plural = "is";
-    }
+	constructor(options) {
+		if (options.plural === true) {
+			this.plural = "are";
+		} else {
+			this.plural = "is";
+		}
 
-    if (options.name === false || options.name === undefined) {
-      this.name = "You";
-      this.plural = "are";
-    } else {
-      this.name = options.name;
-    }
+		if (options.name === false || options.name === undefined) {
+			this.name = "You";
+			this.plural = "are";
+		} else {
+			this.name = options.name;
+		}
 
-    if (options.name.toLowerCase() === "they") {
-      this.plural = "are";
-    }
+		if (this.name.toLowerCase() === "they") {
+			this.plural = "are";
+		}
 
-    this.options = options;
-  }
-  Get(callback) {
-    var t = {
-      adjective: [
-        wordList.adjective[
-          Math.floor(Math.random() * wordList.adjective.length)
-        ],
-        wordList.adjective[
-          Math.floor(Math.random() * wordList.adjective.length)
-        ],
-        wordList.adjective[
-          Math.floor(Math.random() * wordList.adjective.length)
-        ],
-      ],
-      amount:
-        wordList.amount[Math.floor(Math.random() * wordList.amount.length)],
-      animal:
-        wordList.animal[Math.floor(Math.random() * wordList.animal.length)],
-      animal_part:
-        wordList.animal_part[
-          Math.floor(Math.random() * wordList.animal_part.length)
-        ],
-    };
-    var sentance = `${this.name} ${this.plural} ${t.adjective[0]} as a ${t.adjective[1]} ${t.amount} of ${t.adjective[2]} ${t.animal} ${t.animal_part}`;
+		this.options = options;
+	}
+	Get(callback) {
+		var t = {
+			adjective: [
+				wordList.adjective[
+					Math.floor(Math.random() * wordList.adjective.length)
+				],
+				wordList.adjective[
+					Math.floor(Math.random() * wordList.adjective.length)
+				],
+				wordList.adjective[
+					Math.floor(Math.random() * wordList.adjective.length)
+				],
+			],
+			amount: wordList.amount[
+				Math.floor(Math.random() * wordList.amount.length)
+			],
+			animal: wordList.animal[
+				Math.floor(Math.random() * wordList.animal.length)
+			],
+			animal_part:
+				wordList.animal_part[
+					Math.floor(Math.random() * wordList.animal_part.length)
+				],
+		};
+		var sentance = `${this.name} ${this.plural} ${t.adjective[0]} as a ${t.adjective[1]} ${t.amount} of ${t.adjective[2]} ${t.animal} ${t.animal_part}`;
 
-    var result = {
-      sentance,
-      name: this.options.name,
-    };
+		var result = {
+			sentance,
+			name: this.name,
+		};
 
-    if (typeof callback === "function") {
-      callback(result);
-    }
+		if (typeof callback === "function") {
+			callback(result);
+		}
 
-    return result;
-  }
+		return result;
+	}
 }
 
 export default Roastra;
